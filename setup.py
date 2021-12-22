f5823a5769b0f326ee635c971b40ae1f854733b2
Robert <robbybparker@gmail.com>
2021-12-22 16:01:55 -0500

    add setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name="dae_testing",
+    version="0.1.0",
+    description="Testing improvements to Pyomo DAE and incidence analysis",
+    author="Robert Parker and Sakshi Naik",
+    packages=["dae_testing"],
+)
